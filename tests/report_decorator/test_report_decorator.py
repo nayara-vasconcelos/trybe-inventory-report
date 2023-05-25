ec1f963c2d06ede993114f7c87bf353688f21ce2
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-05-25 19:40:38 -0300

    test: testa ColoredReport para relatorio simples

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,47 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    products = [
+        {
+            "id": "1",
+            "nome_do_produto": "Nicotine Polacrilex",
+            "nome_da_empresa": "Target Corporation",
+            "data_de_fabricacao": "2021-02-18",
+            "data_de_validade": "2023-02-17",
+            "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+            "instrucoes_de_armazenamento": "instrucao 1",
+        },
+        {
+            "id": "2",
+            "nome_do_produto": "fentanyl citrate",
+            "nome_da_empresa": "Target Corporation",
+            "data_de_fabricacao": "2020-12-06",
+            "data_de_validade": "2023-12-25",
+            "numero_de_serie": "FR29 5951 7573 74OY XKGX 6CSG D20",
+            "instrucoes_de_armazenamento": "instrucao 2",
+        },
+        {
+            "id": "3",
+            "nome_do_produto": "NITROUS OXIDE",
+            "nome_da_empresa": "Galena Biopharma",
+            "data_de_fabricacao": "2020-12-22",
+            "data_de_validade": "2024-11-07",
+            "numero_de_serie": "CZ09 8588 0858 8435 9140 2695",
+            "instrucoes_de_armazenamento": "instrucao 3",
+        },
+    ]
+
+    expected_simple_report = (
+        "\033[32mData de fabricação mais antiga:\033[0m"
+        " \033[36m2020-12-06\033[0m\n"
+        "\033[32mData de validade mais próxima:\033[0m"
+        " \033[36m2023-12-25\033[0m\n"
+        "\033[32mEmpresa com mais produtos:\033[0m"
+        " \033[31mTarget Corporation\033[0m"
+    )
+
+    colored_simple_report = ColoredReport(SimpleReport).generate(products)
+
+    assert colored_simple_report == expected_simple_report
